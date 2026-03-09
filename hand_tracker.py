7b9443e099baaeb264e40fb9b4f28df8ba9ccd2a
joseclaudioals109@gmail.com <joseclaudioals109@gmail.com>
2026-03-08 23:46:30 -0300

    Primeiro Commit - Teste de Camera

---
+++ hand_tracker.py
@@ -0,0 +1,17 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)
+cam.set(4, 480)
+
+while True:
+    sucesso, img=cam.read()
+    if not sucesso:
+        break
+
+    cv2.imshow('img', cv2.resize(img, (640, 480)))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
