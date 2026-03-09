b715665737ee6dc28e251ebaf5842118deb6951d
joseclaudioals109@gmail.com <joseclaudioals109@gmail.com>
2026-03-09 13:07:42 -0300

    Função de desenhar as marcas na mao

---
+++ draw.py
@@ -0,0 +1,52 @@
+# função provida do google
+
+import numpy as np
+import cv2
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # verde vibrante
+
+
+# Função auxiliar oficial do Google para desenhar os landmarks
+def draw_landmarks_on_image(rgb_image, detection_result):
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = np.copy(rgb_image)
+
+    # Itera sobre todas as mãos detectadas na imagem
+    for idx in range(len(hand_landmarks_list)):
+        hand_landmarks = hand_landmarks_list[idx]
+        handedness = handedness_list[idx]
+
+        # Converte as coordenadas para o formato Protobuf que a função de desenho nativa exige
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+        ])
+
+        # Desenha os nós e as conexões da mão
+        solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            hand_landmarks_proto,
+            solutions.hands.HAND_CONNECTIONS,
+            solutions.drawing_styles.get_default_hand_landmarks_style(),
+            solutions.drawing_styles.get_default_hand_connections_style()
+        )
+
+        # Encontra o topo da mão para desenhar o texto (Left/Right)
+        height, width, _ = annotated_image.shape
+        x_coordinates = [landmark.x for landmark in hand_landmarks]
+        y_coordinates = [landmark.y for landmark in hand_landmarks]
+        text_x = int(min(x_coordinates) * width)
+        text_y = int(min(y_coordinates) * height) - MARGIN
+
+        # Escreve se é mão esquerda ou direita
+        cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+
+    return annotated_image
